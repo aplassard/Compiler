e3b97692ae300c9a1f1b5feaaca471290583f596
plassaaw <plassaaw@550-0106.CEAS1.uc.edu>
2013-01-17 12:13:43 -0500

    Added initial structure and CompilerMain.java

---
+++ Compiler/src/edu/uc/compiler/CompilerMain.java
@@ -0,0 +1,13 @@
+package edu.uc.compiler;
+
+public class CompilerMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is the main method for the compiler class");
+
+	}
+
+}
