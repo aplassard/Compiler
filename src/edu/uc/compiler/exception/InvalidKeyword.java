575f5140dc2f1b9a92d19589a3914b83a56a74a4
aplassard <andrew.plassard@gmail.com>
2013-01-24 12:42:39 -0500

    Added InvalidKeyword Exception

---
+++ src/edu/uc/compiler/exception/InvalidKeyword.java
@@ -0,0 +1,15 @@
+package edu.uc.compiler.exception;
+
+public class InvalidKeyword extends BadToken {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8475448352186144830L;
+
+	public InvalidKeyword(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
