478cb0957997218122005d6c5d923e3fe8e44524
aplassard <andrew.plassard@gmail.com>
2013-02-21 14:53:29 -0500

    Initialized Parser

---
+++ src/edu/uc/compiler/parser/Parser.java
@@ -0,0 +1,13 @@
+package edu.uc.compiler.parser;
+
+import edu.uc.compiler.lexicalanalysis.*;
+
+public class Parser {
+
+	private LexicalAnalyzer lex;
+	
+	public Parser(LexicalAnalyzer LA) {
+		this.lex = LA;
+	}
+
+}
